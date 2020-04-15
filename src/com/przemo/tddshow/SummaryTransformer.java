d0105bfbd68c349d0ad8facc65596949bb205eae
przemo <p@g.com>
2020-04-15 08:37:04 +0200

    assertThrows for transform(null) -> Green

--- src/com/przemo/tddshow/SummaryTransformer.java
+++ src/com/przemo/tddshow/SummaryTransformer.java
@@ -1,7 +1,9 @@
 package com.przemo.tddshow;
 
 public class SummaryTransformer {
-    public String transform(String order) {
+    public String transform(String order) throws IllegalArgumentException {
+        if (order == null)
+            throw new IllegalArgumentException("Invalid order value: null");
         return "ResultString";
     }
 }
